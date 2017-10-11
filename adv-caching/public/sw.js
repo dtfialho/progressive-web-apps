90e5d510d74f6b1d88750583c088a9c70b04fcd3
Diego T. Fialho <diego.tfialho@gmail.com>
2017-10-11 12:09:01 -0300

    Cache then network and dynamic caching

--- adv-caching/public/sw.js
+++ adv-caching/public/sw.js
@@ -44,6 +44,19 @@ self.addEventListener('activate', function(event) {
   return self.clients.claim();
 });
 
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.open(CACHE_DYNAMIC_NAME)
+      .then(function(cache) {
+        return fetch(event.request)
+          .then(function(res) {
+            cache.put(event.request, res.clone());
+            return res;
+          });
+      })
+  );
+});
+
 // self.addEventListener('fetch', function(event) {
 //   event.respondWith(
 //     caches.match(event.request)
@@ -70,21 +83,21 @@ self.addEventListener('activate', function(event) {
 //   );
 // });
 
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    fetch(event.request)
-      .then(function(res) {
-        return caches.open(CACHE_DYNAMIC_NAME)
-          .then(function(cache) {
-            cache.put(event.request.url, res.clone());
-            return res;
-          });
-      })
-      .catch(function(err) {
-        return caches.match(event.request);
-      })
-  );
-});
+// self.addEventListener('fetch', function(event) {
+//   event.respondWith(
+//     fetch(event.request)
+//       .then(function(res) {
+//         return caches.open(CACHE_DYNAMIC_NAME)
+//           .then(function(cache) {
+//             cache.put(event.request.url, res.clone());
+//             return res;
+//           });
+//       })
+//       .catch(function(err) {
+//         return caches.match(event.request);
+//       })
+//   );
+// });
 
 // cache only
 // self.addEventListener('fetch', function(event) {
